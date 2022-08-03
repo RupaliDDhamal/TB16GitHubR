3ac99f320707db19415646685bd470072db3212e
RupaliDDhamal <110520356+RupaliDDhamal@users.noreply.github.com>
2022-08-03 12:25:59 +0530

    by rupali
    for testing

--- AutoGitHub1/src/com/GitHubDemo/GithubDemo.java
+++ AutoGitHub1/src/com/GitHubDemo/GithubDemo.java
@@ -1,8 +1,10 @@
-package com.GitHubDemo;
+kage com.GitHubDemo;
 
 public class GithubDemo
 {
+	
 public static void main(String[] args) {
+	System.out.println("Hello");
 	System.out.println("Hello");
 }
 }
