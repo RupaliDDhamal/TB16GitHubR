e16d399d0a899a243fecfc6d5e8cf50a1cc8410d
NehaWargantiwar <110392680+NehaWargantiwar@users.noreply.github.com>
2022-08-03 12:46:45 +0530

    by neha
    for testing

--- AutoGitHub1/src/com/GitHubDemo/GithubDemo.java
+++ AutoGitHub1/src/com/GitHubDemo/GithubDemo.java
@@ -2,7 +2,10 @@ kage com.GitHubDemo;
 
 public class GithubDemo
 {
-	
+	public void m3()
+	{
+		sytem.out.println("m3 start")
+	}
 public static void main(String[] args) {
 	System.out.println("Hello");
 	System.out.println("Hello");
